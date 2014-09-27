f61f1d99002d19f9b66ae7f5a7b37368e3b154f3
Malkavia <malkavia@MalkaviadeMacBook-Pro.local>
2014-09-27 15:24:53 +0800

    Initial Commit

---
+++ XMStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMStudy
+//
+//  Created by Malkavia on 14-9-27.
+//  Copyright (c) 2014年 Malkavia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XMStudy
+//
+//  Created by Malkavia on 14-9-27.
+//  Copyright (c) 2014年 Malkavia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
